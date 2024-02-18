cd8c4d32f5691910652b29e47744b1a50dcd2035
Varsha <you@example.com>
2024-02-17 23:04:35 -0500

    updated instructions

---
+++ src/Footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom">
+    <div className="center">All rights reserved Danish Uddin Mohammed</div>
+  </footer>
+  );
+}
+
+export default Footer;
